c7e1c2564c3476ab3f98ed35568a8c7f88ba2b98
gn.fur <patych7@gmail.com>
2018-03-24 13:58:33 +0300

    Created project.

---
+++ Flucoldache/Flucoldache/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Flucoldache
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			Console.WriteLine("Foxes are fluffers. ╦════════");
+			
+			while(true)
+			{
+				char ch = Console.ReadKey(true).KeyChar;
+				Console.Write(ch.ToString());
+			}
+		}
+	}
+}
